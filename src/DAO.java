e12e3f0b8ddf468a1fb54153606f72492d9e23f6
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-03-29 09:24:22 +0200

    Initial commit

---
+++ src/DAO.java
@@ -0,0 +1,8 @@
+public interface DAO {
+
+    boolean insertText(int id, String text);    // Create
+    String getText(int id);                     // Read
+    boolean updateText(int id, String text);    // Update
+    void deleteText(int id);                    // Delete
+
+}
