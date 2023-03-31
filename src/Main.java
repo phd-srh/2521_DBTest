7b157794659655f750a1272be5a1f4f485b73e50
Dr. Dillinger <peter.dillinger@srh.de>
2023-03-31 11:55:29 +0200

    Ein paar Tests ergänzt...

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         DAO db = new TempDAO();
+        showDB(db);
 
+        db.deleteText(3);
+        db.insertText(99, "Roman");
+        showDB(db);
+
+        System.out.println( db.getText(99) );
+
+        db.updateText(99, "Computermagazin");
+        showDB(db);
+    }
+
+    private static void showDB(DAO db) {
         for (String text : db.getAll()) {
             System.out.println(text);
         }
-
+        System.out.println("--------------------");
     }
 }
